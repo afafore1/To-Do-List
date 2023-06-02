af409563f3911438bf65c5fe11e034ae3f8cb966
Ayomitunde Fafore <ayo@streamlit.io>
2023-06-01 21:16:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.write('Hello')
+def main():
+    st.title("To-Do List App")
+
+    task_list = st.session_state.get('task_list', [])
+
+    new_task = st.text_input("New Task")
+
+    if new_task:
+        task_list.append(new_task)
+        st.session_state['task_list'] = task_list
+
+    if st.button("Clear Tasks"):
+        task_list.clear()
+        st.session_state['task_list'] = task_list
+
+    for task in task_list:
+        st.write(task)
+
+if __name__ == "__main__":
+    main()
